27d369b621372997b613f2cd0dc6c7f09dfbc7a8
Lucas Cid <lucasfelipelimacid@gmail.com>
2023-11-19 14:57:51 -0300

    table users updated

--- learning-about-creating-api/src/database/sqlite/migrations/createUser.js
+++ learning-about-creating-api/src/database/sqlite/migrations/createUser.js
@@ -2,7 +2,6 @@ const createUsers = `
     CREATE TABLE IF NOT EXISTS users2 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name varchar not null,
-    cpf integer not null,
     email VARCHAR,
     password VARCHAR,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
